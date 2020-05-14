fc58a546d6b4a6dff7f32f456aafed4bb966253b
tychejin1218 <tychejin@naver.com>
2020-05-14 17:30:28 +0900

    BoardController 구현 및 테스트

--- src/main/java/com/api/board/controller/BoardController.java
+++ src/main/java/com/api/board/controller/BoardController.java
@@ -2,13 +2,15 @@ package com.api.board.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.api.board.domain.Board;
 import com.api.board.service.BoardService;
@@ -17,16 +19,96 @@ import com.api.board.service.BoardService;
 @Controller
 public class BoardController {
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
 	@Autowired
 	private BoardService boardService;
 
+	/**
+	 * 게시글 목록 조회
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	public List<Board> getBoardList() throws Exception {
 		List<Board> boardList = boardService.getBoardList();
 
 		return boardList;
+	}
+
+	/**
+	 * 게시글 상세 조회
+	 * 
+	 * @param board_seq
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/{board_seq}", method = RequestMethod.GET)
+	@ResponseBody
+	public Board getBoardDetail(@PathVariable("board_seq") int board_seq) throws Exception {
+
+		Board board = boardService.getBoardDetail(board_seq);
+
+		return board;
+	}
+
+	/**
+	 * 게시글 등록 
+	 * 
+	 * @param board
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(value = HttpStatus.CREATED)
+	@ResponseBody
+	public Board insertBoard(@RequestBody Board board) throws Exception {
+
+		boardService.insertBoard(board);
+
+		int boardSeq = board.getBoard_seq();
+
+		Board boardDetail = boardService.getBoardDetail(boardSeq);
+
+		return boardDetail;
+	}
+
+	/**
+	 * 게시글 수정
+	 * 
+	 * @param board
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/{board_seq}", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
+	public Board updateBoard(@PathVariable("board_seq") int board_seq, @RequestBody Board board) throws Exception {
+
+		boardService.updateBoard(board);
+
+		Board boardDetail = boardService.getBoardDetail(board_seq);
+
+		return boardDetail;
+	}
+
+	/**
+	 * 게시글 삭제 
+	 * 
+	 * @param board_seq
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/{board_seq}", method = RequestMethod.DELETE)
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
+	public Board deleteBoard(@PathVariable("board_seq") int board_seq) throws Exception {
+
+		boardService.deleteBoard(board_seq);
+
+		Board deleteBoard = new Board();
+		deleteBoard.setBoard_seq(board_seq);
+
+		return deleteBoard;
 	}
 }
