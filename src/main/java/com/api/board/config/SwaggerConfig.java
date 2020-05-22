ca13fe1ebed008fc121b61a7dc5d1a733d51f242
tychejin1218 <tychejin@naver.com>
2020-05-22 19:12:01 +0900

    Swagger 설정

---
+++ src/main/java/com/api/board/config/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package com.api.board.config;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).consumes(getConsumeContentTypes())
+			.produces(getProduceContentTypes())
+			.apiInfo(getApiInfo())
+			.select()
+			.apis(RequestHandlerSelectors.basePackage("com.api.board.controller"))
+			.paths(PathSelectors.ant("/board/**"))
+			.build();
+	}
+
+	private Set<String> getConsumeContentTypes() {
+		Set<String> consumes = new HashSet<>();
+		consumes.add("application/json;charset=UTF-8");
+		consumes.add("application/x-www-form-urlencoded");
+		return consumes;
+	}
+
+	private Set<String> getProduceContentTypes() {
+		Set<String> produces = new HashSet<>();
+		produces.add("application/json;charset=UTF-8");
+		return produces;
+	}
+
+	private ApiInfo getApiInfo() {
+		return new ApiInfoBuilder().title("REST API 게시판 만들기")
+			.description("API Docs")
+			.version("1.0")
+			.build();
+	}
+
+	/**
+	 * 404 Not Found가 발생하는 경우 swagger-ui.html 위치를 추가
+	 */
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+			.addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**")
+			.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
