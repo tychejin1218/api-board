660c38b6943662bc759939287d5ae038ebe02960
tychejin1218 <tychejin@naver.com>
2020-06-03 18:37:38 +0900

    lombok 적용

--- src/main/java/com/api/board/domain/Board.java
+++ src/main/java/com/api/board/domain/Board.java
@@ -5,11 +5,14 @@ import javax.xml.bind.annotation.XmlType;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 @ApiModel
 @XmlRootElement(name = "board")
-@XmlType(propOrder = { "board_seq", "board_re_ref", "board_re_lev", "board_re_seq", "board_writer", "board_subject", "board_content", "board_hits", "del_yn", "ins_user_id", "ins_date", "upd_user_id",
-		"upd_date" })
+@XmlType(propOrder = { "board_seq", "board_re_ref", "board_re_lev", "board_re_seq", "board_writer", "board_subject", "board_content", "board_hits", "del_yn", "ins_user_id", "ins_date", "upd_user_id", "upd_date" })
+@Getter
+@Setter
 public class Board {
 
 	@ApiModelProperty(value = "게시글 번호")
@@ -38,108 +41,4 @@ public class Board {
 	String upd_user_id;
 	@ApiModelProperty(value = "게시글 수정 일시")
 	String upd_date;
-
-	public int getBoard_seq() {
-		return board_seq;
-	}
-
-	public void setBoard_seq(int board_seq) {
-		this.board_seq = board_seq;
-	}
-
-	public int getBoard_re_ref() {
-		return board_re_ref;
-	}
-
-	public void setBoard_re_ref(int board_re_ref) {
-		this.board_re_ref = board_re_ref;
-	}
-
-	public int getBoard_re_lev() {
-		return board_re_lev;
-	}
-
-	public void setBoard_re_lev(int board_re_lev) {
-		this.board_re_lev = board_re_lev;
-	}
-
-	public int getBoard_re_seq() {
-		return board_re_seq;
-	}
-
-	public void setBoard_re_seq(int board_re_seq) {
-		this.board_re_seq = board_re_seq;
-	}
-
-	public String getBoard_writer() {
-		return board_writer;
-	}
-
-	public void setBoard_writer(String board_writer) {
-		this.board_writer = board_writer;
-	}
-
-	public String getBoard_subject() {
-		return board_subject;
-	}
-
-	public void setBoard_subject(String board_subject) {
-		this.board_subject = board_subject;
-	}
-
-	public String getBoard_content() {
-		return board_content;
-	}
-
-	public void setBoard_content(String board_content) {
-		this.board_content = board_content;
-	}
-
-	public int getBoard_hits() {
-		return board_hits;
-	}
-
-	public void setBoard_hits(int board_hits) {
-		this.board_hits = board_hits;
-	}
-
-	public String getDel_yn() {
-		return del_yn;
-	}
-
-	public void setDel_yn(String del_yn) {
-		this.del_yn = del_yn;
-	}
-
-	public String getIns_user_id() {
-		return ins_user_id;
-	}
-
-	public void setIns_user_id(String ins_user_id) {
-		this.ins_user_id = ins_user_id;
-	}
-
-	public String getIns_date() {
-		return ins_date;
-	}
-
-	public void setIns_date(String ins_date) {
-		this.ins_date = ins_date;
-	}
-
-	public String getUpd_user_id() {
-		return upd_user_id;
-	}
-
-	public void setUpd_user_id(String upd_user_id) {
-		this.upd_user_id = upd_user_id;
-	}
-
-	public String getUpd_date() {
-		return upd_date;
-	}
-
-	public void setUpd_date(String upd_date) {
-		this.upd_date = upd_date;
-	}
 }
